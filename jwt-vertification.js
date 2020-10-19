1f856a2625c5c5d7d7a8a08f730efc0765674e3b
zhaoxiaoqi <snailzhao1995@163.com>
2020-10-19 22:28:15 +0800

    :construction: add jwt

--- jwt-vertification.js
+++
@@ -1,9 +0,0 @@
-function jwtVertification() {
-  const cookie = window.document.cookie;
-  console.log("jwtVertification -> cookie", cookie)
-  return false;
-}
-
-module.exports = {
-  vertify: jwtVertification,
-};
--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 let express = require('express');
 let path = require('path');
+let jwt = require('jsonwebtoken');
+
 let expressHttpProxy = require('express-http-proxy');
-let vertify = require('./jwt-vertification').vertify;
+let cors = require('cors');
+
+const token = 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWUsImlhdCI6MTUxNjIzOTAyMn0.POstGetfAytaZS82wHcjoTyoqhMyxXiWdR7Nn7A29DNSl0EiXLdwJ6xC6AfgZWF1bOsS_TuYI3OG85AmiExREkrS6tDfTQ2B3WXlrr-wp5AokiRbz3_oB4OxG-W9KcEEbDRcZc0nH3L7LzYptiy1PtAylQGxHTWZXtGz4ht0bAecBgmpdgXMguEIcoqPJ1n3pIWk_dUZegpqx0Lka21H6XxUTxiy8OcaarA8zdnPUnV6AmNP3ecFawIFYdvJB_cm-GvpCSbr8G8y_Mllj8f4x9nBH8pQux89_6gUY618iYv7tuPWBFfEbLxtF2pZS6YC1aSfLQxeNe8djT9YjpvRZA';
 
 const config = require(path.resolve(__dirname, 'proxy-conf.js'));
 const port = config.port || 80;
@@ -9,16 +13,35 @@ const proxyConfig = config.proxy || {};
 
 let app = express();
 
+app.use(cors());
+
 Object.keys(proxyConfig).forEach(key => {
   app.use(key, expressHttpProxy(
     proxyConfig[key].target,
     {
       ...proxyConfig[key],
       filter: function(req, rep) {
+        console.log(req.headers.cookie);
+        jwt.verify(token, `-----BEGIN PUBLIC KEY-----
+MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAnzyis1ZjfNB0bBgKFMSv
+vkTtwlvBsaJq7S5wA+kzeVOVpVWwkWdVha4s38XM/pa/yr47av7+z3VTmvDRyAHc
+aT92whREFpLv9cj5lTeJSibyr/Mrm/YtjCZVWgaOYIhwrXwKLqPr/11inWsAkfIy
+tvHWTxZYEcXLgAXFuUuaS3uF9gEiNQwzGTU1v0FqkqTBr4B8nW3HCN47XUu0t8Y0
+e+lf4s4OxQawWD79J9/5d3Ry0vbV3Am1FtGJiJvOwRsIfVChDpYStTcHTCMqtvWb
+V6L11BWkpzGXSW4Hv43qa+GSYOD2QU68Mb59oSk2OB+BtOLpJofmbGEGgvmwyCI9
+MwIDAQAB
+-----END PUBLIC KEY-----`,
+        { algorithms: ['RS256'], complete: true },
+        function(err, decoded) {
+          console.log(err, decoded);
+
+        });
+        const result = jwt.decode(token);
+        // console.log('wew', result);
         // const isVerified = vertify();
-        // if (!isVerified) {
-        //   rep.send('verify failed');
-        // }
+        if (result) {
+          rep.send('verify failed');
+        }
         return true;
     }}
   ));
